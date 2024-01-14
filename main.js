5b7f0dbf4c673a4d92fa1622b3a1f3ee7df9411e
SantiagoMarcucci <marcuccisantiago8@gmail.com>
2024-01-13 21:33:14 -0300

    realizado todo el desafio, falta testear

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+const fs = require("fs");
+
 class ProductManager {
   static ultId = 0;
 
-  constructor() {
+  constructor(path) {
     this.products = [];
+    this.path = path;
   }
 
-  addProduct(tittle, description, price, img, code, stock) {
+  async addProduct(tittle, description, price, img, code, stock) {
     if (!tittle || !description || !price || !img || !code || !stock) {
       console.log("Todos los campos son obligatorios");
       return;
@@ -27,19 +30,52 @@ class ProductManager {
     };
 
     this.products.push(newProduct);
+
+    await this.saveFile(this.products);
   }
 
-  getProducts() {
-    return this.products;
+  async getProducts() {
+    try {
+      const respuesta = await fs.promises.readFile(this.path, "utf-8");
+      const arrayProducts = JSON.parse(respuesta);
+      return arrayProducts;
+    } catch (error) {
+      console.log("Error al leer el archivo", error);
+    }
+    //return this.products;
   }
 
-  getProductsById(id) {
-    const product = this.products.find((item) => item.id === id);
+  async getProductsById(id) {
+    try {
+      const product = await this.getProducts();
+      const productFind = product.find((item) => item.id === id);
+    } catch (error) {
+      console.log("Product Not found", error);
+    }
+  }
 
-    if (!product) {
-      console.error("Not Found");
-    } else {
-      console.log(product);
+  async updateProducts(id, product) {
+    let data = await this.getProducts();
+    let i = data.findIndex((e) => e.id === id);
+    data.splice(i, 1, product);
+  }
+
+  async deleteProducts(id){
+    const data = await this.getProducts();
+    let i = data.findIndex((e) => e.id === id);
+    data.splice(i, 1);
+    await fs.promises.writeFile(this.path, JSON.stringify(data));
+    
+  }
+
+  async saveFile(arrayProducts) {
+    try {
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(arrayProducts, null, 2)
+      );
+    } catch (error) {
+      console.log("Error al guardar el archivo, vamos a morir", error);
     }
   }
 }
@@ -60,15 +96,14 @@ manager.addProduct(
 //console.log(manager.getProducts());
 
 manager.addProduct(
-    "Producto prueba",
-    "esto es un producto prueba",
-    500,
-    "Sin imagen",
-    "abc123",
-    20
-  );
-  
-  console.log(manager.getProducts());
+  "Producto prueba",
+  "esto es un producto prueba",
+  500,
+  "Sin imagen",
+  "abc123",
+  20
+);
 
-  manager.getProductsById(2)
+console.log(manager.getProducts());
 
+manager.getProductsById(2);
